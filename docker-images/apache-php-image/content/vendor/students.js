6a76e9052c116c9fda09ef06cb3fca5fa14cd00e
MICHAEL-DELL728\HEIG-Michael <michael.dasilva@heig-vd.ch>
2019-05-29 23:21:19 +0200

    Added JS script to send AJAX requests to fetch students and update DOM

---
+++ docker-images/apache-php-image/content/vendor/students.js
@@ -0,0 +1,19 @@
+$(function() {
+   console.log("Loading students");
+
+
+   function loadStudents() {
+      $.getJSON("/api/students/", function(students) {
+         console.log(students);
+         var message = "Nobody is here";
+         if (students.length > 0) {
+            message = students[0].firstName + " " + 
+students[0].lastName;
+         }
+         $(".navbar-brand").text(message);
+      });
+   };
+
+   loadStudents();
+   setInterval(loadStudents, 2000);
+});
