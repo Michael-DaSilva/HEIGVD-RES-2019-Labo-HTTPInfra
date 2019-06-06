4d5a55eecad4aa1354c1388b6fc35a443153dbe1
MICHAEL-DELL728\HEIG-Michael <michael.dasilva@heig-vd.ch>
2019-06-06 09:11:56 +0200

    Modification of the content sent by HTTP request

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send( generateStudents() );
+    res.send( generateHashtags() );
 });
 
 app.get('/test', function(req,res){
@@ -16,30 +16,21 @@ app.listen(3000, function(){
     console.log('Accepting HTTP requests on port 3000');
 });
 
-function generateStudents(){
-  var numberOfStudents = chance.integer({
+function generateHashtags(){
+  var numberOfHashtags = chance.integer({
     min: 0,
     max: 10
   });
-  console.log(numberOfStudents);
-  var students = [];
-  for (var i = 0; i < numberOfStudents; i++) {
-    var gender = chance.gender();
-    var birthYear = chance.year({
-      min: 1986,
-      max: 1996
-    });
-    students.push({
-      firstName: chance.first({
-        gender: gender
-      }),
-      lastName: chance.last(),
-      gender: gender,
-      birthday: chance.birthday({
-        year: birthYear
-      })
+  console.log(numberOfHashtags);
+  var hashtags = [];
+  for (var i = 0; i < numberOfHashtags; i++) {
+    var expr = chance.hashtag();
+    var user = chance.twitter();
+    hashtags.push({
+      hashtag: expr,
+	  from: user
     });
   };
-  console.log(students);
-  return students;
+  console.log(hashtags);
+  return hashtags;
 }
